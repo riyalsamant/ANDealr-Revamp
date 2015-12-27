8d94690192b73026e99a96a292ff46e0c59c8d3d
riyalsamant <samantriyal@gmail.com>
2015-12-27 19:05:38 +0530

    About us layout

---
+++ app/src/main/java/com/ascentsmartwaves/andealrrevamp/activities/AboutUsActivity.java
@@ -0,0 +1,26 @@
+package com.ascentsmartwaves.andealrrevamp.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import com.ascentsmartwaves.andealrrevamp.R;
+
+public class AboutUsActivity extends Activity {
+TextView t,t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+
+    }
+
+}
